7ff06931ef05db7c370f36625d2c57649090deb0
zofiagrodecka <69384237+zofiagrodecka@users.noreply.github.com>
2021-01-19 14:06:02 +0100

    Added a trigger

--- triggers.sql
+++ triggers.sql
@@ -25,3 +25,20 @@ BEGIN
 	SET EndDate = DATEADD(WEEK, 2, StartDate)
 	WHERE EndDate > DATEADD(WEEK, 2, StartDate)
 END
+GO
+
+CREATE TRIGGER CanMakeReservationTrigger
+ON Reservations
+FOR INSERT 
+AS
+BEGIN
+	DECLARE @customerid AS INT
+	SET @customerid = (SELECT i.CustomerID FROM INSERTED AS i)
+	DECLARE @orderid AS INT
+	SET @orderid = (SELECT i.OrderID FROM INSERTED AS i)
+	IF (SELECT dbo.CanMakeReservation(@customerid, @orderid, 50.0, 5, 200.0)) = 0
+	BEGIN
+		RAISERROR ('This customer does not have the right to make reservation', -1, -1)
+		ROLLBACK TRANSACTION
+	END
+END
