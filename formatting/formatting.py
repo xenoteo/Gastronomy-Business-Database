e209f20f5cabd6def2f9c6ec25d64d4297e2ad95
xenoteo <fedorovxa@gmail.com>
2021-01-21 20:55:45 +0100

    Documenting added

--- formatting/formatting.py
+++ formatting/formatting.py
@@ -2,14 +2,29 @@ import os
 
 
 class Formatter:
-    def __init__(self, filename, new_filename):
-        self.filename = filename
-        self.new_filename = new_filename
-        self.KEYWORDS = ['int', 'nvarchar', 'varchar', 'null', 'bit', 'datetime', 'float', ' on ', 'set', ' table ', 'select', 'from']
+    """" Class responsible for changing the format of SQL script. """
+
+    def __init__(self, filename):
+        self.filename = filename        # the path to the file to edit
+        self.temp_filename = filename   # the path to the temp buffer file
+        self.set_temp_file()
+        self.KEYWORDS = ['int', 'nvarchar', 'varchar', 'null', 'bit', 'datetime', 'float', ' on ', 'set',
+                         ' table ', 'select', 'from']   # a list of keywords to set uppercase
+
+    def set_temp_file(self):
+        """
+        A function that sets the path to the temp buffer file
+        adding to the filename of the main file prefix 'tmp_'.
+        """
+
+        index = self.filename.rfind('/') + 1
+        self.temp_filename = self.filename[:index] + "tmp_" + self.filename[index:]
 
     def uppercase(self):
+        """ A function looking for keywords and setting them uppercase. """
+
         file = open(self.filename, 'r')
-        new_file = open(self.new_filename, 'w')
+        new_file = open(self.temp_filename, 'w')
         for line in file:
             for keyword in self.KEYWORDS:
                 if keyword in line:
@@ -20,8 +35,10 @@ class Formatter:
         self.overwrite_file()
 
     def capitalize(self):
+        """ A function looking for SQL variables and capitalizing them.  """
+
         file = open(self.filename, 'r')
-        new_file = open(self.new_filename, 'w')
+        new_file = open(self.temp_filename, 'w')
         for line in file:
             new_line = ""
             for i in range(len(line)):
@@ -35,30 +52,23 @@ class Formatter:
         self.overwrite_file()
 
     def clear_go(self):
+        """ A function removing all the GOs from the script. """
+
         file = open(self.filename, 'r')
-        new_file = open(self.new_filename, 'w')
+        new_file = open(self.temp_filename, 'w')
         for line in file:
-            if "GO" not in line:
+            if "GO" not in line and "go" not in line:
                 new_file.write(line)
         file.close()
         new_file.close()
         self.overwrite_file()
 
-    def add_go(self):
-        file = open(self.filename, 'r')
-        new_file = open(self.new_filename, 'w')
-        for line in file:
-            new_file.write(line)
-            if "IF EXISTS" in line:
-                new_file.write("GO\n")
-        file.close()
-        new_file.close()
-        self.overwrite_file()
-
     def overwrite_file(self):
-        new_file = open(self.new_filename, 'r')
+        """ A function that writes the content of the buffer file to the main file and removes the buffer file. """
+
+        new_file = open(self.temp_filename, 'r')
         file = open(self.filename, 'w')
         file.writelines(new_file.readlines())
         new_file.close()
         file.close()
-        os.remove(self.new_filename)
+        os.remove(self.temp_filename)
--- formatting/main.py
+++ formatting/main.py
@@ -1,7 +1,6 @@
 from formatting import Formatter
 
-formatter = Formatter('../functions.sql', '../new_functions.sql')
+formatter = Formatter('../functions.sql')
+
 formatter.capitalize()
 formatter.uppercase()
-formatter.clear_go()
-formatter.add_go()
